33660ce1398810afa84721df77c1bc5b6efd8bbb
marcelolimabh <marcelolimabh@gmail.com>
2017-12-30 16:54:36 -0200

    Filtrar  Autores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { AutorService } from './autor/autor.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 
 import { AppComponent } from './app.component';
+import { AutorService } from './autor/autor.service';
 import { TestData } from './shared/test-data';
 import { AutorComponent } from './autor/autor.component';
-import { HttpClientModule } from '@angular/common/http';
+
 
 
 @NgModule({
@@ -18,6 +21,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     InMemoryWebApiModule.forRoot(TestData)
 
   ],
--- src/app/autor/autor.component.ts
+++ src/app/autor/autor.component.ts
@@ -1,8 +1,11 @@
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 
 import { AutorService } from './autor.service';
 import { Livro } from './../shared/models/livro';
+import { Autor } from '../shared/models/autor';
 
 
 @Component({
@@ -15,13 +18,28 @@ export class AutorComponent implements OnInit {
 
   obsTextMsg: Observable<string>;
   obsLivros: Observable<Livro[]>;
+  livros: Livro[];
+  livrosFavoritos: Livro[];
+  autorFavorito: Autor;
+  autor: Autor;
+
+  categorias = [
+    { categoria: 'Angular' },
+    { categoria: 'Hibernate' },
+    { categoria: 'Java' }
+  ];
+  anos = [
+    { ano: '2014' },
+    { ano: '2015' },
+    { ano: '2016' }
+  ];
 
 
-  constructor(private autorService: AutorService) { }
+  constructor(private autorService: AutorService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-  this.getTextMsg();
-  this.getLivros();
+    this.getTextMsg();
+    this.getLivros();
 
   }
 
@@ -30,12 +48,27 @@ export class AutorComponent implements OnInit {
     this.obsTextMsg = this.autorService.getTextoMensagem();
   }
 
-  getLivros(){
+  getLivros() {
     this.obsLivros = this.autorService.getLivros();
   }
 
+  livroForm = this.formBuilder.group({
+    categoria: '',
+    ano: ''
+  });
 
 
+  onFormSubmit() {
+    let categoria: string = this.livroForm.get('categoria').value;
+    let ano: string = this.livroForm.get('ano').value;
+    this.filtraLivros(categoria, ano);
+  }
+
+
+  filtraLivros(categoria: string, ano: string): void {
+    this.autorService.filtrarLivros(categoria, ano).
+      subscribe(data => this.livros = data);
+  }
 
 
 
